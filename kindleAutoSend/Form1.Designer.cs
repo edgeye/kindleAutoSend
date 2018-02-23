5768323fa89428234fcaaf29e2bdf0b8ffaeaf1a
叶若闻 <edgeye@163.com>
2018-02-23 11:54:21 +0800

    kindle推送程序

--- kindleAutoSend/Form1.Designer.cs
+++ kindleAutoSend/Form1.Designer.cs
@@ -28,12 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnchoosefile = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSendren = new System.Windows.Forms.TextBox();
+            this.txtSendMail = new System.Windows.Forms.TextBox();
+            this.txtSendMailPsd = new System.Windows.Forms.TextBox();
+            this.txtReMail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSendContent = new System.Windows.Forms.TextBox();
+            this.txtBooks = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnchoosefile
+            // 
+            this.btnchoosefile.Location = new System.Drawing.Point(20, 75);
+            this.btnchoosefile.Name = "btnchoosefile";
+            this.btnchoosefile.Size = new System.Drawing.Size(98, 25);
+            this.btnchoosefile.TabIndex = 0;
+            this.btnchoosefile.Text = "添加推送书籍：";
+            this.btnchoosefile.UseVisualStyleBackColor = true;
+            this.btnchoosefile.Click += new System.EventHandler(this.btnchoosefile_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(403, 361);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(196, 34);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "开始推送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "收件邮箱：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "发送人：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "发送邮箱密码：";
+            // 
+            // txtSendren
+            // 
+            this.txtSendren.Location = new System.Drawing.Point(127, 36);
+            this.txtSendren.Name = "txtSendren";
+            this.txtSendren.Size = new System.Drawing.Size(322, 25);
+            this.txtSendren.TabIndex = 6;
+            this.txtSendren.Text = "叶若闻";
+            // 
+            // txtSendMail
+            // 
+            this.txtSendMail.Location = new System.Drawing.Point(127, 69);
+            this.txtSendMail.Name = "txtSendMail";
+            this.txtSendMail.Size = new System.Drawing.Size(322, 25);
+            this.txtSendMail.TabIndex = 7;
+            this.txtSendMail.Text = "kfwcw2000@163.com";
+            // 
+            // txtSendMailPsd
+            // 
+            this.txtSendMailPsd.Location = new System.Drawing.Point(127, 103);
+            this.txtSendMailPsd.Name = "txtSendMailPsd";
+            this.txtSendMailPsd.Size = new System.Drawing.Size(322, 25);
+            this.txtSendMailPsd.TabIndex = 8;
+            // 
+            // txtReMail
+            // 
+            this.txtReMail.Location = new System.Drawing.Point(124, 28);
+            this.txtReMail.Name = "txtReMail";
+            this.txtReMail.Size = new System.Drawing.Size(321, 25);
+            this.txtReMail.TabIndex = 10;
+            this.txtReMail.Text = "@kindle.cn";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "发送邮箱：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtTitle);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtSendContent);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtSendMail);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtSendMailPsd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtSendren);
+            this.groupBox1.Location = new System.Drawing.Point(12, 18);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 294);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "发件信息";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtBooks);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnchoosefile);
+            this.groupBox2.Controls.Add(this.txtReMail);
+            this.groupBox2.Location = new System.Drawing.Point(501, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(475, 283);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "收件信息";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "发送内容：";
+            // 
+            // txtSendContent
+            // 
+            this.txtSendContent.Location = new System.Drawing.Point(127, 180);
+            this.txtSendContent.Multiline = true;
+            this.txtSendContent.Name = "txtSendContent";
+            this.txtSendContent.Size = new System.Drawing.Size(322, 95);
+            this.txtSendContent.TabIndex = 10;
+            this.txtSendContent.Text = "书";
+            // 
+            // txtBooks
+            // 
+            this.txtBooks.Location = new System.Drawing.Point(20, 117);
+            this.txtBooks.Multiline = true;
+            this.txtBooks.Name = "txtBooks";
+            this.txtBooks.Size = new System.Drawing.Size(440, 147);
+            this.txtBooks.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(66, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "标题：";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(127, 137);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(322, 25);
+            this.txtTitle.TabIndex = 12;
+            this.txtTitle.Text = "书";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(51, 318);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(925, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 326);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(992, 407);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnSend);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "kindle书籍自动推送小程序";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnchoosefile;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSendren;
+        private System.Windows.Forms.TextBox txtSendMail;
+        private System.Windows.Forms.TextBox txtSendMailPsd;
+        private System.Windows.Forms.TextBox txtReMail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtSendContent;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBooks;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- kindleAutoSend/Form1.cs
+++ kindleAutoSend/Form1.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +20,192 @@ namespace kindleAutoSend
         {
             InitializeComponent();
         }
+
+        private MailMessage mailMessage;
+        private SmtpClient smtpClient;
+        private string password;//发件人密码 
+
+        private void btnchoosefile_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.InitialDirectory = "c:\\";//注意这里写路径时要用c:\\而不是c:\
+            //openFileDialog.Filter = "文本文件|*.*|C#文件|*.cs|所有文件|*.*";
+            //openFileDialog.RestoreDirectory = true;
+            //openFileDialog.FilterIndex = 1;
+            //if (openFileDialog.ShowDialog() == DialogResult.OK)
+            //{
+
+
+            //    //this.textBox1.Text = openfiled;
+
+            //}
+            this.txtBooks.Text = this.txtBooks.Text == "" ? SelectPathFile() : this.txtBooks.Text + "," + SelectPathFile();
+
+        }
+
+        public void SendMail(string To, string From, string Body, string Title, string Password)
+        {
+            mailMessage = new MailMessage();
+            mailMessage.To.Add(To);
+            mailMessage.From = new System.Net.Mail.MailAddress(From);
+            mailMessage.Subject = Title;
+            mailMessage.Body = Body;
+            mailMessage.IsBodyHtml = true;
+            mailMessage.BodyEncoding = System.Text.Encoding.UTF8;
+            mailMessage.Priority = System.Net.Mail.MailPriority.Normal;
+            this.password = Password;
+        }
+        /// <summary>  
+        /// 添加附件  
+        /// </summary>  
+        public void Attachments(string Path)
+        {
+            string[] path = Path.Split(',');
+            Attachment data;
+            ContentDisposition disposition;
+            for (int i = 0; i < path.Length; i++)
+            {
+                data = new Attachment(path[i], MediaTypeNames.Application.Octet);//实例化附件  
+                //disposition = data.ContentDisposition;
+                //disposition.CreationDate = System.IO.File.GetCreationTime(path[i]);//获取附件的创建日期  
+                //disposition.ModificationDate = System.IO.File.GetLastWriteTime(path[i]);//获取附件的修改日期  
+                //disposition.ReadDate = System.IO.File.GetLastAccessTime(path[i]);//获取附件的读取日期  
+                mailMessage.Attachments.Add(data);//添加到附件中  
+            }
+        }
+        /// <summary>  
+        /// 异步发送邮件  
+        /// </summary>  
+        /// <param name="CompletedMethod"></param>  
+        public void SendAsync(SendCompletedEventHandler CompletedMethod)
+        {
+            if (mailMessage != null)
+            {
+                smtpClient = new SmtpClient();
+                smtpClient.Credentials = new System.Net.NetworkCredential(mailMessage.From.Address, password);//设置发件人身份的票据  
+                smtpClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                smtpClient.Host = "smtp." + mailMessage.From.Host;
+                smtpClient.SendCompleted += new SendCompletedEventHandler(CompletedMethod);//注册异步发送邮件完成时的事件  
+                smtpClient.SendAsync(mailMessage, mailMessage.Body);
+            }
+        }
+        /// <summary>  
+        /// 发送邮件  
+        /// </summary>  
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            fThread = new Thread(new ThreadStart(SleepT));//开辟一个新的线程  
+            fThread.Start();
+            this.btnSend.Enabled = false;
+
+
+
+            string to = txtReMail.Text;
+            string from = txtSendMail.Text;
+            string body = txtSendContent.Text;
+            string title = txtTitle.Text;
+            string password = txtSendMailPsd.Text;
+            SendMail(to, from, body, title, password);
+            string path = this.txtBooks.Text;
+            if (path != null && !path.Equals(""))
+            {
+                Attachments(path);
+            }
+
+
+            if (mailMessage != null)
+            {
+                smtpClient = new SmtpClient();
+                smtpClient.Credentials = new System.Net.NetworkCredential(mailMessage.From.Address, password);//设置发件人身份的票据  
+                smtpClient.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                smtpClient.Host = "smtp." + mailMessage.From.Host;
+                smtpClient.Send(mailMessage);
+            }
+
+            
+        }
+
+        // 选择文件：
+        private string SelectPathFile()
+        {
+            string path = string.Empty;
+            var openFileDialog = new OpenFileDialog()
+            {
+                Filter = "Files (*.*)|*.*"//如果需要筛选txt文件（"Files (*.txt)|*.txt"）
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = openFileDialog.FileName;
+            }
+            return path;
+        }
+        // 选择路径
+        private string SelectPath()
+        {
+            string path = string.Empty;
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+            }
+            return path;
+        }
+
+
+        //更新进度列表  
+        private delegate void SetPos(int ipos);
+
+        private void SetTextMessage(int ipos)
+        {
+            if (this.InvokeRequired)
+            {
+                SetPos setpos = new SetPos(SetTextMessage);
+                this.Invoke(setpos, new object[] { ipos });
+            }
+            else
+            {
+                this.label7.Text = ipos.ToString() + "%";
+                this.progressBar1.Value = Convert.ToInt32(ipos);
+            }
+        }
+
+        public Thread fThread;
+
+        private void SleepT()
+        {
+            for (int i = 0; i <= 500; i++)
+            {
+                System.Threading.Thread.Sleep(10); //线程休眠，控制进度条的速度。  
+                SetTextMessage(100 * i / 500);
+
+            }
+            Control.CheckForIllegalCrossThreadCalls = false;
+            if (progressBar1.Value == 100)
+            {
+                if (MessageBox.Show("已完成！", "提示", MessageBoxButtons.OK) == DialogResult.OK)
+                {
+                    progressBar1.Value = 0;
+                    this.label7.Text = "0%";
+                    this.btnSend.Enabled = true;
+                }
+            }
+
+        }
+
+        //不能在form主线程以外的线程中调用 控件，所以必须用委托，暂时用 Control.CheckForIllegalCrossThreadCalls = false;这个方法越过检查
+        ////主线程
+        //public delegate void UpdateMessage(string mes);
+        //public void UpdatePortMessage(string mes)
+        //{
+        //    this.txtMessage.AppendText(string.Format("Connected {0}. succeed", mes) + "\r\n");
+        //    this.txtClientIP.Text = mes;
+        //}
+
+
+        //// 线程1
+        //UpdateMessage upPort = new UpdateMessage(UpdatePortMessage);
+        //                this.BeginInvoke(upPort, point);
     }
 }
